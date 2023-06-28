29413c3a7c0549cd814e8e2e1c84a2a5b7c57350
ChristopherPHolder <toholderandrews@gmail.com>
2023-06-28 21:56:37 +0200

    add user-flow

--- packages/user-flow/src/lib/user-flow.spec.ts
+++ packages/user-flow/src/lib/user-flow.spec.ts
@@ -3,5 +3,5 @@ import { userFlow } from './user-flow';
 describe('userFlow', () => {
   it('should work', async () => {
     expect(await userFlow()).toEqual('user-flow');
-  });
+  }, 50_000);
 });
--- packages/user-flow/src/lib/user-flow.ts
+++ packages/user-flow/src/lib/user-flow.ts
@@ -1,9 +1,11 @@
 import {launch, Browser, Page} from 'puppeteer';
-import {startFlow} from 'lighthouse'
+import {startFlow, UserFlow} from 'lighthouse';
 
 export async function userFlow(): Promise<string> {
-  const browser = await launch()
-  const page = await browser.newPage();
+  const browser: Browser = await launch({headless: false})
+  const page: Page = await browser.newPage();
+  const flow: UserFlow = await startFlow(page);
+  await flow.navigate('https://push-based.io');
   console.log('WOLOLO');
   await browser.close();
   return 'user-flow';
